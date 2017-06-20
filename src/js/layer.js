ef34f4d42d7c6831e9d868930f7835cf5573c7dc
Zzzz <281295062@qq.com>
2017-06-20 14:52:28 +0800

    ui更新

--- src/js/layer.js
+++ src/js/layer.js
@@ -5,7 +5,7 @@
  */
 function setParam(data) {
 	//弹框类型（“1” 普通弹框， “2” 进度条弹框，“3” 支付弹框 ）
-	//var data = Base64.decode(JSON.stringify(GetQueryString('json')))
+	var data = Base64.decode(JSON.stringify(GetQueryString('json')))
 	var _data = JSON.parse(data);
 
 	if (_data.type === "1") {
@@ -437,4 +437,4 @@ function _off() {
 
 	$('.land').off();
 }
-//setParam()
+setParam()
